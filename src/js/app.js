e9ff2962ccb6992eb9b093146af4d936baa5f053
Evgeniy-27 <Li.Evgen@yandex.ru>
2021-01-30 20:18:40 +0700

    исправления линт

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
 // TODO: write code here
 
 // comment this to pass build
-const unusedVariable = 'variable';
 
 // for demonstration purpose only
 export default function demo(value) {
